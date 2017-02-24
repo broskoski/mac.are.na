3c040852a068ad251825d7bc532e54136d3dc419
Charles Broskoski <broskoski@gmail.com>
2017-02-24 16:13:37 -0500

    Add basic home and playlist app

---
+++ src/containers/home.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import { apiBase, playlistChannel } from '../config'
+import PlaylistLink from '../components/playlist_link'
+import Header from '../components/header'
+
+class Home extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      page: 1,
+      per: 20,
+      playlists: [],
+    }
+  }
+  componentWillMount() {
+    const component = this
+    fetch(`${apiBase}/channels/${playlistChannel}/contents?page=${this.state.page}&per=${this.state.per}`)
+      .then(function(response) {
+        return response.json();
+      }).then(function(response) {
+        const playlists = response.contents;
+        component.setState({ playlists });
+      }).catch(function(ex) {
+        console.log('parsing failed', ex);
+      })
+  }
+  render () {
+    let playlists = []
+    for (var i=0; i < this.state.playlists.length; i++) {
+      playlists.push(
+        <PlaylistLink
+          key={this.state.playlists[i].id} 
+          playlist={this.state.playlists[i]} 
+        />
+      );
+    }
+    return (
+      <div className='w-100 min-vh-100 pa3 pa5-ns'>
+        <Header />
+        {playlists}
+      </div>
+    )
+  }
+}
+
+export default Home
---
+++ src/containers/playlist.js
@@ -0,0 +1,64 @@
+import React from 'react'
+import { find } from 'lodash'
+import { apiBase } from '../config'
+import { onlySongs } from '../lib/filter'
+import Header from '../components/header'
+import PlaylistDisplay from '../components/playlist_display'
+import PlaylistItem from '../components/playlist_item'
+
+class Playlist extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      items: [],
+      playlistID: null,
+      selectedID: null
+    }
+  }
+  componentDidMount() {
+    const component = this
+    const playlistID = this.props.params.playlistID 
+    
+    this.setState({ playlistID })
+
+    fetch(`${apiBase}/channels/${playlistID}/contents`)
+      .then(function(response) {
+        return response.json();
+      }).then(function(response) {
+        const items = onlySongs(response.contents);
+        component.setState({ items });
+      }).catch(function(ex) {
+        console.log('parsing failed', ex);
+      })
+  }
+  render () {
+    let items = []
+    for (var i=0; i < this.state.items.length; i++) {
+      items.push(
+        <PlaylistItem
+          key={this.state.items[i].id} 
+          item={this.state.items[i]}
+          isSelected={this.state.items[i].id == this.state.selectedID}
+          onPress={(id) => {
+            console.log('onPress', id, 'this', this)
+            this.setState({ selectedID: id }) 
+          }}
+        />
+      );
+    }
+
+    const selectedItem = find(this.state.items, (item) => {
+      return item.id == this.state.selectedID
+    })
+    
+    return (
+      <div className='w-100 min-vh-100 pa3 pa5-ns'>
+        <Header />
+        <PlaylistDisplay item={selectedItem} />
+        {items}
+      </div>
+    )
+  }
+}
+
+export default Playlist
