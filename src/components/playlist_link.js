1081456043958968691106619ac25106a573468d
hxrts <sam@hxrts.com>
2017-03-02 02:39:52 -0500

    adds pagination, activePage doesn't work quite right with playlist loading

--- src/components/playlist_link.js
+++ src/components/playlist_link.js
@@ -4,8 +4,14 @@ import { Link } from 'react-router'
 class PlaylistLink extends React.Component {
   render () {
     const playlist = this.props.playlist
+    const itemClass = "bb pv3 bg-animate"
+    const selectedClass = (
+      this.props.isSelected ?
+      itemClass + ' bg-light-pink ' :
+      itemClass + ' hover-bg-washed-red'
+    )
     return (
-      <div className="bb pv3">
+      <div  className={selectedClass}>
         <Link className="list-item link dim black" to={`/playlist/${playlist.slug}`}>
           {playlist.user.full_name} / {playlist.title}
         </Link>
