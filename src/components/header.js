63609375f54043fc71b6b3e8bf8efdf1f3537f63
Charles Broskoski <broskoski@gmail.com>
2017-03-17 12:18:31 -0400

    Update link

--- src/components/header.js
+++ src/components/header.js
@@ -5,7 +5,7 @@ class Header extends React.Component {
   render() {
     return (
       <h1>
-        <a href="https://are.na/mac-are-na/" target="_blank">
+        <a href="https://www.are.na/mac-are-na/" target="_blank">
           <img className="logo dim black" src={require('../assets/logo.svg')} alt="logo" />
         </a>
         <Link className="link dim black" to={`/`}>
